7dfb26a6288cbe4e16980ac234b2d5dee61cb18f
ThirumaleshFSD <sirisampadha939184@gmail.com>
2026-04-30 23:15:04 +0530

    two pointer problems

---
+++ TwoPointerDuplicate.java
@@ -0,0 +1,21 @@
+public class TwoPointerDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 5, 6,1};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            if (arr[left] == arr[right]) {
+                System.out.println("Duplicate found: " + arr[left]);
+                return;
+            } else if (arr[left] < arr[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println("No duplicate found.");
+    }
+}
---
+++ TwoPointerSum.java
@@ -0,0 +1,24 @@
+public class TwoPointerSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int target = 7;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println("Pair found: " + arr[left] + " + " + arr[right] + " = " + target);
+                return;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println("No pair found that sums to " + target);
+    }
+}
