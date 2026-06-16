ea3285eb223bfd8c659510780d5d443d6b91a792
ThirumaleshFSD <sirisampadha939184@gmail.com>
2026-06-16 23:41:43 +0530

    binary search proble

---
+++ binarySearch.java
@@ -0,0 +1,32 @@
+public class binarySearch {
+   public static void main(String[] args) {
+        int[] sortedArray = {1, 3, 5, 7, 9, 11, 13};
+        int target = 7;
+        int result = binarySearch(sortedArray, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+
+        return -1; // Target not found
+    } 
+}
