d380737ed25a292a9eed55721b5c9b35437d00f0
ThirumaleshFSD <sirisampadha939184@gmail.com>
2026-07-28 22:37:31 +0530

    the string problem added

---
+++ pob1.java
@@ -0,0 +1,9 @@
+public class pob1{
+    public static void main(String[] args) {
+        String s="  thi ru ";
+        String s1[]=s.split(" ");
+        for(String str:s1){
+            System.out.print(str);
+        }
+    }
+}
